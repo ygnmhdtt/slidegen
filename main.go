7cb376b1a0db0708120f265e94c30cfec5371b99
ygnmhdtt <ygnmhdtt@gmail.com>
2018-06-22 11:23:47 +0900

    fix using wkhtmltopdf.NewPage instead of NewPageReader

--- main.go
+++ main.go
@@ -24,36 +24,52 @@ func main() {
 	check(err)
 
 	mds := splitMarkdownFiles(os.Args[1])
-	for _, md := range mds {
-		html := genHTML(md)
+	htmls := genHTML(mds)
+	for _, html := range htmls {
 		pdfg.AddPage(wkhtmltopdf.NewPage(html))
 	}
-
 	check(pdfg.Create())
 	check(pdfg.WriteFile("output.pdf"))
 }
 
 // genHTML generates html from given markdown file
-func genHTML(markdownPath string) string {
-	markdownFile, err := ioutil.ReadFile(markdownPath)
-	check(err)
+func genHTML(markdownPathes []string) []string {
+	var files []string
 
-	//HTMLFlags and Renderer
-	htmlFlags := blackfriday.CommonHTMLFlags //UseXHTML | Smartypants | SmartypantsFractions | SmartypantsDashes | SmartypantsLatexDashes
-	// htmlFlags |= blackfriday.FootnoteReturnLinks     //Generate a link at the end of a footnote to return to the source
-	// htmlFlags |= blackfriday.SmartypantsAngledQuotes //Enable angled double quotes (with Smartypants) for double quotes rendering
-	// htmlFlags |= blackfriday.SmartypantsQuotesNBSP   //Enable French guillemets 損 (with Smartypants)
-	renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{Flags: htmlFlags, Title: "", CSS: ""})
+	for i, markdownPath := range markdownPathes {
+		markdownFile, err := ioutil.ReadFile(markdownPath)
+		check(err)
 
-	//Extensions
-	extFlags := blackfriday.CommonExtensions //NoIntraEmphasis | Tables | FencedCode | Autolink | Strikethrough | SpaceHeadings | HeadingIDs | BackslashLineBreak | DefinitionLists
-	// extFlags |= blackfriday.Footnotes        //Pandoc-style footnotes
-	// extFlags |= blackfriday.HeadingIDs       //specify heading IDs  with {#id}
-	// extFlags |= blackfriday.Titleblock       //Titleblock ala pandoc
-	// extFlags |= blackfriday.DefinitionLists  //Render definition lists
+		//HTMLFlags and Renderer
+		htmlFlags := blackfriday.CommonHTMLFlags //UseXHTML | Smartypants | SmartypantsFractions | SmartypantsDashes | SmartypantsLatexDashes
+		// htmlFlags |= blackfriday.FootnoteReturnLinks     //Generate a link at the end of a footnote to return to the source
+		// htmlFlags |= blackfriday.SmartypantsAngledQuotes //Enable angled double quotes (with Smartypants) for double quotes rendering
+		// htmlFlags |= blackfriday.SmartypantsQuotesNBSP   //Enable French guillemets 損 (with Smartypants)
+		renderer := blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{Flags: htmlFlags, Title: "", CSS: ""})
 
-	html := blackfriday.Run(markdownFile, blackfriday.WithExtensions(extFlags), blackfriday.WithRenderer(renderer))
-	return string(html)
+		//Extensions
+		extFlags := blackfriday.CommonExtensions //NoIntraEmphasis | Tables | FencedCode | Autolink | Strikethrough | SpaceHeadings | HeadingIDs | BackslashLineBreak | DefinitionLists
+		// extFlags |= blackfriday.Footnotes        //Pandoc-style footnotes
+		// extFlags |= blackfriday.HeadingIDs       //specify heading IDs  with {#id}
+		// extFlags |= blackfriday.Titleblock       //Titleblock ala pandoc
+		// extFlags |= blackfriday.DefinitionLists  //Render definition lists
+
+		html := blackfriday.Run(markdownFile, blackfriday.WithExtensions(extFlags), blackfriday.WithRenderer(renderer))
+
+		filename := fmt.Sprintf("page%d.html", i)
+		f, err := os.Create(filename)
+		check(err)
+
+		defer f.Close()
+
+		w := bufio.NewWriter(f)
+		_, err = w.WriteString(string(html))
+		check(err)
+		w.Flush()
+
+		files = append(files, filename)
+	}
+	return files
 }
 
 // makeMarkdownFiles prepare markdown files
